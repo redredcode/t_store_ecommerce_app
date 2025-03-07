044d841f05681940fd37d4a6d9ce98a92f7dc082
redwanahmedfahim <thebigredbhuiyan@gmail.com>
2025-03-08 02:16:26 +0600

    define api, color, image string, sizes, and texts constants, create helper functions, create formatter, create device utilities

---
+++ lib/utils/helpers/helper_functions.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:intl/intl.dart';
+
+class THelperFunctions {
+  static Color? getColor(String value) {
+    /// Define your product specific colors here and it will match the attribute colors and show specific
+    if (value == 'Green') {
+      return Colors.green;
+    } else if (value == 'Green') {
+      return Colors.green;
+    } else if (value == 'Red') {
+      return Colors.red;
+    } else if (value == 'Blue') {
+      return Colors.blue;
+    } else if (value == 'Pink') {
+      return Colors.pink;
+    } else if (value == 'Grey') {
+      return Colors.grey;
+    } else if (value == 'Purple') {
+      return Colors.purple;
+    } else if (value == 'Black') {
+      return Colors.black;
+    } else if (value == 'White') {
+      return Colors.white;
+    } else if (value == 'Brown') {
+      return Colors.brown;
+    } else if (value == 'Teal') {
+      return Colors.teal;
+    } else if (value == 'Indigo') {
+      return Colors.indigo;
+    } else {
+      return null;
+    }
+  }
+
+  static void showSnackBar(String message) {
+    ScaffoldMessenger.of(Get.context!).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  //static void showAlert
+
+
+}
