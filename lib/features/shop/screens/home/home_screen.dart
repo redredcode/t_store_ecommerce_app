fb19c22a8f6be9310337d72a4128eda72c2611d6
redwanahmedfahim <thebigredbhuiyan@gmail.com>
2025-03-14 03:23:05 +0600

    fix circle design not appearing, create reusable appbar

--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -1,40 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:t_store_ecommerce_app/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:t_store_ecommerce_app/common/widgets/appbar/appbar.dart';
 import 'package:t_store_ecommerce_app/utils/constants/colors.dart';
-
-import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
+import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ClipPath(
-              clipper: TCustomCurvedEdges(),
-              child: Container(
-                color: TColors.primary,
-                padding: const EdgeInsets.all(0),
-                child: SizedBox(
-                  height: 400,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                          top: -150,
-                          right: -250,
-                          child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1),)),
-                      Positioned(
-                          top: 100,
-                          right: -300,
-                          child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1),)),
-                    ],
-                  ),
-                ),
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  THomeAppBar(),
+                ],
               ),
-            )
+            ),
           ],
         ),
       ),
