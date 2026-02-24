60b0db9544b0777efa5ef3fc9cac54ed40b77357
fahim <thebigredbhuiyan@gmail.com>
2026-02-24 06:33:20 +0600

    feat: complete cart screen

---
+++ lib/features/shop/screens/cart/cart_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:t_store_ecommerce_app/common/widgets/appbar/appbar.dart';
+import 'package:t_store_ecommerce_app/common/widgets/texts/product_price_text.dart';
+import 'package:t_store_ecommerce_app/utils/constants/sizes.dart';
+import '../../../../common/widgets/products/cart/add_remove_button.dart';
+import '../../../../common/widgets/products/cart/cart_item.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TAppbar(
+        title: Text('Cart', style: Theme.of(context).textTheme.headlineSmall),
+        showBackArrow: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ListView.separated(
+          shrinkWrap: true,
+          itemCount: 12,
+          separatorBuilder: (_, __) => const SizedBox(height: TSizes.spaceBtwItems),
+          itemBuilder: (_, index) => const Column(
+            children: [
+              TCartItem(),
+              SizedBox(height: TSizes.spaceBtwItems),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 70),
+                      TProductQuantityWithAddRemoveButton(),
+                    ],
+                  ),
+                  /// Add and Remove button
+                  TProductPriceText(price: '256')
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ElevatedButton(onPressed: (){}, child: const Text('Checkout \$256.0')),
+      ),
+    );
+  }
+}
